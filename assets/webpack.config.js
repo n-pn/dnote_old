675c112c619f8557dff26ec181607f9a3865fc84
nipinium <nipinium@gmail.com>
2018-12-08 19:42:58 +0700

    Fix scss

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -29,7 +29,7 @@ module.exports = (env, options) => ({
         }
       },
       {
-        test: /\.css$/,
+        test: /\.scss$/,
         use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
       }
     ]
